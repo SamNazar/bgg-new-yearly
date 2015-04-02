a745c79e22c5d97e646c758c252a41826dedc604
Sam N <worldsaddress@yahoo.com>
2015-04-02 11:57:37 -0700

    Cleaned out unused code

--- bgg-new-to-you-year.rb
+++ bgg-new-to-you-year.rb
@@ -7,6 +7,8 @@
 # The modified script merely lists the new games a user has logged in a given calendar year
 # and provides a count of those games.  This is to keep track of challenges like 51-in-15
 # 
+# TODO: If year is not current year, end date should be Dec 31 of that year
+# TODO: It seems that this is making a new request for each new game, fix this
 # 
 
 require 'date'
@@ -15,28 +17,21 @@ require 'open-uri'
 require 'nokogiri'
 require './game'
 
-class NewToYou
+class NewToYouYear
   def initialize
-    last_month = Date.today << 1
+    todays_date = Date.today
     @options = {
       :username     => 'sam n',
-      :month        => last_month.month,
-      :year         => last_month.year,
+      :year         => todays_date.year,
     }
 
     parse_options
 
-    # Establish previous start and end dates
-    last_month = Date.new(@options[:year], @options[:month])
-    #@options[:start_date] = (last_month - 1).to_s
     ## start at beginning of year given
     @options[:start_date] = Date.parse(@options[:year].to_s + "-01-01")
 
-    # last_month >> 1 gets the same time as above + 1 month, - 1 subtracts a day
-    #@options[:end_date] = ((last_month >> 1) - 1).to_s
-
     ## end at today
-    @options[:end_date] = Date.today
+    @options[:end_date] = todays_date
 
     print_games(retrieve_plays())
   end
@@ -45,24 +40,20 @@ class NewToYou
   def parse_options
     OptionParser.new do |opts|
       opts.banner = "Retrieve a listing of games that were new to you.
-    Usage: bgg-new-to-you.rb --username wesbaker --month 6"
+    Usage: bgg-new-to-you-year.rb --username UserName --year 2015"
 
       opts.on('-u username', '--username username', "Username") do |username|
         @options[:username] = username.to_s
       end
 
-      opts.on('-m MONTH', '--month MONTH', "Month (numeric, e.g. 5 or 12)") do |month|
-        @options[:month] = month.to_i
-      end
-
-      opts.on('-y YEAR', '--year YEAR', 'Year (four digits, e.g. 2013)') do |year|
+      opts.on('-y YEAR', '--year YEAR', 'Year (four digits, e.g. 2015)') do |year|
         @options[:year] = year.to_i
       end
     end.parse!
   end
 
   def retrieve_plays(start_date = @options[:start_date], end_date = @options[:end_date], username = @options[:username])
-    # Retrieve games played in month
+    # Retrieve games played in year
     plays = BGG_API.new('plays', {
       :username => username,
       :mindate  => start_date,
@@ -72,7 +63,7 @@ class NewToYou
 
     _games = Hash.new
 
-    # First, get this month's plays
+    # First, get this year's plays
     plays.css('plays > play').each do |play|
       quantity = play.attr('quantity')
       item = play.search('item')
@@ -92,6 +83,7 @@ class NewToYou
 
     _games.each do |objectid, data|
       # Filter out games I've played before (before mindate)
+      puts "Making BGG Request for previous plays"
       previous_plays = BGG_API.new('plays', {
         :username => username,
         :maxdate  => start_date,
@@ -102,71 +94,20 @@ class NewToYou
         _games.delete(objectid)
         next
       end
-
-      # Now, figure out what my current ratings and plays for that game is
-#      game_info = BGG_API.new('collection', {
-#        :username => username,
-#        :id       => objectid,
-#        :stats    => 1
-#      }).retrieve
-
-      # Error out
-#      if not game_info.at_css('rating')
-#        game_info = BGG_API.new('thing', {
-#          :id       => objectid
-#        }).retrieve
-#        name = game_info.css('name').first['value']
-#        puts "#{name} not rated. Rate the game and run this script again:"
-#        puts "\thttp://boardgamegeek.com/collection/user/#{username}?played=1&rated=0&ff=1"
-#      end
-
-#      if game_info.at_css('rating').is_a? Nokogiri::XML::Element
-#        _games[objectid][:rating] = game_info.css('rating').attr('value').content.to_i
-#        _games[objectid][:comment] = game_info.css('comment').text
-#        _games[objectid][:imageid] = game_info.css('image').text.match(/\d+/)[0].to_i
-
-        #Figure out plays since
-#        total_plays = game_info.css('numplays').first.text.to_i
-#        _games[objectid][:plays_since] = total_plays - _games[objectid][:plays]
-#      else
-#        _games[objectid][:rating] = 0
-#        _games[objectid][:comment] = ''
-#        _games[objectid][:plays_since] = 0
-#      end
     end
-
-    # Sort games by rating
-    _games.sort_by { |objectid, data| data[:rating] * -1 }
   end
 
   def print_games(_games)
-    # Print each game's name and number 
+    # Print each game's name
     _games.each do |objectid, data|
       puts data[:name]
-      #puts data.render
     end
     # print total number of new games this year
     puts _games.length.to_s + " new games played in " + @options[:year].to_s + "."
   end
 
-  def print_plays(_games)
-    # Spit out something coherent
-    _games.each do |objectid, data|
-      data[:stars] = ':star:' * data[:rating] + ':nostar:' * (10 - data[:rating])
-      data[:play_count] = play_count(data[:plays], data[:plays_since])
-      puts data.render
-    end
-  end
-
-  def play_count(plays, since)
-    text = "#{plays} play"
-    text += 's' if plays > 1
-    text += ", #{since} since" if since > 0
-    text
-  end
-
   public :initialize
-  private :parse_options, :retrieve_plays, :print_plays, :play_count
+  private :parse_options, :retrieve_plays
 end
 
 # BGG API class that pulls in data and takes a hash as a set of options for the
@@ -204,4 +145,4 @@ class BGG_API
   end
 end
 
-NewToYou.new
+NewToYouYear.new
