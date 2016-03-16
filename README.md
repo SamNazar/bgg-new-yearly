30f325e1e9d0338dcf39166d9bd812612b0a5616
Sam N <worldsaddress@yahoo.com>
2016-03-15 18:51:18 -0700

    add ability to sort results and show more information
    add an —order parameter to allow sorting of the results
    add a —verbose parameter to show more information
    Update README.md to reflect feature changes.

--- README.md
+++ README.md
@@ -1,32 +1,46 @@
-BoardGameGeek New to You This Year Script
-=========================================
+#BoardGameGeek New to You This Year Script
 
 I wanted to see how many new games I'd played this year.  This script will list your new games logged on BGG in the last year and give you a total.
 
-This is a modified version of wesbaker's script which he wrote for the monthly New-to-You GeekList.  It was modified to handle the larger number of plays that occur in a year vs. a month.  Big thanks to Wes for the original.
+This started out as a modified version of @wesbaker's script, which he wrote for the monthly New-to-You GeekList.  It was modified to handle the larger number of plays that can occur in a year vs. a month.  Big thanks to Wes for the original.
 
-Installing
-----------
+## Installing
 
 First and foremost, you'll need at least Ruby 1.9.2 due to Nokogiri. After that,
 you should use [bundler](http://bundler.io) to install the required gems:
 
-    bundle install
+```
+bundle install
+```
 
 You might also have to change permissions to make the script executable:
 
-    chmod +x bgg-new-to-you-year.rb
+```
+chmod +x bgg-new-to-you-year.rb
+```
 
-Using
------
+## Using
 
 Once everything's loaded, run the script like so:
 
-    ./bgg-new-to-you-year.rb --username <your_username>
+```
+./bgg-new-to-you-year.rb --username <your_username>
+```
     
 Remember to put your username in quotes if it includes a space.
 
 By default the script will retrieve plays for the current year, if you need to
 pick a different year you can:
 
-    ./bgg-new-to-you-year.rb --username <your_username> --year 2012
+```
+./bgg-new-to-you-year.rb --username <your_username> --year 2012
+```
+
+## Command line options
+* `-u` or `--username`: **REQUIRED**.
+* `-y` or `--year`: The year for which to look up plays.  Default is the current year.
+* `-o` or `--order`: How to order the results.  Default is alphabetically
+    - `alpha`: sort alphabetically by game name
+    - `plays`: descending by number of plays in the given year
+    - `date`: ascending by date of first play 
+* `-v` or `--verbose` will also print number of plays in the given year and the date of the first play.
--- bgg-new-to-you-year.rb
+++ bgg-new-to-you-year.rb
@@ -4,7 +4,7 @@
 # list new games for a particular month and output a skeleton geeklist post with 
 # ratings.
 # 
-# The modified script merely lists the new games a user has logged in a given calendar year
+# The modified script lists the new games a user has logged in a given calendar year
 # and provides a count of those games.  This is to keep track of challenges like 51-in-15
 # 
 
@@ -18,8 +18,9 @@ class NewToYouYear
   def initialize
     todays_date = Date.today
     @options = {
-      :username     => 'sam n',
       :year         => todays_date.year,
+      :order        => :alpha,
+      :verbose      => false,
     }
 
     parse_options
@@ -41,16 +42,28 @@ class NewToYouYear
   def parse_options
     OptionParser.new do |opts|
       opts.banner = "Retrieve a listing of games that were new to you.
-    Usage: bgg-new-to-you-year.rb --username UserName --year 2015"
+    Usage: bgg-new-to-you-year.rb --username 'UserName' --year YYYY --order [alpha, date, plays] --verbose"
 
-      opts.on('-u username', '--username username', "Username") do |username|
+      opts.on('-u username', '--username username', "Username (required)") do |username|
         @options[:username] = username.to_s
       end
 
-      opts.on('-y YEAR', '--year YEAR', 'Year (four digits, e.g. 2015)') do |year|
+      opts.on('-y YEAR', '--year YEAR', Integer, 'Year (four digits, e.g. 2016)') do |year|
         @options[:year] = year.to_i
       end
+
+      opts.on('-o [ORDER]', '--order [ORDER]', [:alpha, :date, :plays], 'Order by (alpha, date, or plays)') do |order|
+        @options[:order] = order
+      end
+
+      opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+        @options[:verbose] = v
+      end
+
     end.parse!
+
+    raise OptionParser::MissingArgument if @options[:username].nil?
+
   end
 
   def retrieve_plays(start_date = @options[:start_date], end_date = @options[:end_date], username = @options[:username])
@@ -82,12 +95,19 @@ class NewToYouYear
         item = play.search('item')
         name = item.attr('name').content
         objectid = item.attr('objectid').content.to_i
+        dateplayed = Date.parse(play.attr('date'))
 
         # Create the hashes if need be
         unless _games.has_key? objectid
           _games[objectid] = Game.new
           _games[objectid][:objectid] = objectid
           _games[objectid][:name] = name
+          _games[objectid][:firstplayed] = dateplayed
+        end
+
+        # if date is earlier than a pre-existing one, set this to the first-played date
+        if (_games.has_key? objectid) and (_games[objectid][:firstplayed] < dateplayed)
+          _games[objectid][:firstplayed] = dateplayed
         end
 
         # Increment play count
@@ -96,7 +116,6 @@ class NewToYouYear
     end
 
     puts "TOTAL PAGES FOR YEAR GIVEN: " + page_num.to_s
-
 
     #make one request for grabbing all games played before given year
     #we can only get 100 plays at a time, so we do this in multiple pages
@@ -131,12 +150,26 @@ class NewToYouYear
   end
 
   def print_games(_games)
-    # sort games by title (name)
-    _games = _games.sort_by { |objectid, data| data[:name] }
+    case @options[:order]
+    when :alpha
+      puts "Listing games alphabetically"
+      # sort games by title (name)
+      _games = _games.sort_by { |objectid, data| data[:name] }
+    when :date
+      puts "Listing gamed by ascending date of first play in the year"
+      _games = _games.sort_by { |objectid, data| data[:firstplayed] }
+    when :plays
+      puts "Listing games by descending number of plays"
+      _games = _games.sort_by { |objectid, data| data[:plays] }.reverse
+    end
     puts "=== New Games Played by " + @options[:username].to_s + " in " + @options[:year].to_s + " ==="
     # Print each game's name
     _games.each do |objectid, data|
       puts data[:name]
+      if @options[:verbose] then
+        puts "  " + data[:plays].to_s + " play(s)"
+        puts "  First played on " + data[:firstplayed].to_s
+      end
     end
     # print total number of new games this year
     puts "=== " + _games.length.to_s + " new games in " + @options[:year].to_s + " ==="
